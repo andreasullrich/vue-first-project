c012012a11ba19a72886f4ca949727137e925782
Andreas Ullrich <ullrich.andi@web.de>
2023-12-12 19:09:09 +0100

    task: Header & BookList Component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
+  <LayoutHeader />
+  <BookList />
+  <MyFirstComponent />
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MyFirstComponent from "./components/MyFirstComponent.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import BookList from "./components/BookList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MyFirstComponent,
+    LayoutHeader,
+    BookList,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+:root {
+  --primary: #6a1cc3;
+  --primary-dark: #370b68;
+  --secondary: #f39c12;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  color: var(--primary);
+  margin-bottom: 15px;
+}
+
+.inside {
+  margin: 10px;
+}
+
+@media only screen and (min-width: 1140px) {
+  .inside {
+    width: 960px;
+    margin: 0 auto;
+  }
 }
 </style>
